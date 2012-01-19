c0717dff74decd19d6c565e34bcd35b7b98f54cf
Thomas Proisl <thomas.proisl@fau.de>
2012-01-19 06:47:36 +0000

    Cluster testen

--- Create/09_create_batch_jobs.sh
+++ Create/09_create_batch_jobs.sh
@@ -66,7 +66,7 @@ wait
 
 for (( i=1 ; i <= $max_n ; i++ ))
 do
-    grep \"\\t\$i\$\" subgraphs_${jobnr}_*.txt > \$FASTTMP/subgraphs_${jobnr}_\$i.txt &
+    grep "\\t\$i\$" subgraphs_${jobnr}_*.txt > \$FASTTMP/subgraphs_${jobnr}_\$i.txt &
 done
 
 wait
