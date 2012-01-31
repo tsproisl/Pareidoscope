24032c4ff567e1b0b92adaaf89f05014fb8a1117
Thomas Proisl <thomas.proisl@fau.de>
2012-01-31 08:17:25 +0000

    Konfiguration für Dancer angefangen

--- Pareidoscope/lib/Pareidoscope.pm
+++ Pareidoscope/lib/Pareidoscope.pm
@@ -1,7 +1,15 @@
 package Pareidoscope;
 use Dancer ':syntax';
 
-our $VERSION = '0.1';
+use Dancer::Plugin::Database;
+use Fcntl ':flock';
+
+our $VERSION = '0.8.1';
+
+hook 'before' => sub {
+    database->sqlite_create_function("rmfile", 1, sub {unlink map("public/cache/$_", @_);});
+    database->sqlite_create_function("logquery", 3, sub {open(OUT, ">>:encoding(utf8)", config->{query_log}) or die("Cannot open query log: $!"); flock(OUT,LOCK_EX); print OUT join("\t", @_), "\n"; flock(OUT,LOCK_UN); close(OUT) or die("Cannot close query log: $!");});
+};
 
 get '/' => sub {
     template 'home';
