104e4efa410d3b51a87a77e77e9d19b95b040b7d
Thomas Proisl <thomas.proisl@fau.de>
2012-02-01 08:09:52 +0000

    Konfiguration

--- Pareidoscope/lib/Pareidoscope.pm
+++ Pareidoscope/lib/Pareidoscope.pm
@@ -7,12 +7,12 @@ use Fcntl ':flock';
 our $VERSION = '0.8.1';
 
 hook 'before' => sub {
-    database->sqlite_create_function("rmfile", 1, sub {unlink map("public/cache/$_", @_);});
-    database->sqlite_create_function("logquery", 3, sub {open(OUT, ">>:encoding(utf8)", config->{query_log}) or die("Cannot open query log: $!"); flock(OUT,LOCK_EX); print OUT join("\t", @_), "\n"; flock(OUT,LOCK_UN); close(OUT) or die("Cannot close query log: $!");});
+    database->sqlite_create_function( "rmfile", 1, sub { unlink map( "public/cache/$_", @_ ); } );
+    database->sqlite_create_function( "logquery", 3, sub { open( OUT, ">>:encoding(utf8)", config->{query_log} ) or die("Cannot open query log: $!"); flock( OUT, LOCK_EX ); print OUT join( "\t", @_ ), "\n"; flock( OUT, LOCK_UN ); close(OUT) or die("Cannot close query log: $!"); } );
 };
 
 get '/' => sub {
-    template 'home';
+    template( 'home', { version => $VERSION } );
 };
 
 true;
