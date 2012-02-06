a8e5d7e4107466117500f59dd1edf96511a34689
Thomas Proisl <thomas.proisl@fau.de>
2012-02-06 08:05:07 +0000

    Weitere Versuche mit Dancer

--- Pareidoscope/lib/Pareidoscope.pm
+++ Pareidoscope/lib/Pareidoscope.pm
@@ -4,34 +4,39 @@ use Dancer ':syntax';
 use Dancer::Plugin::Database;
 use Fcntl ':flock';
 
+use data;
+
 our $VERSION = '0.8.1';
 
 hook 'before' => sub {
     database->sqlite_create_function( "rmfile", 1, sub { unlink map( "public/cache/$_", @_ ); } );
     database->sqlite_create_function( "logquery", 3, sub { open( OUT, ">>:encoding(utf8)", config->{query_log} ) or die("Cannot open query log: $!"); flock( OUT, LOCK_EX ); print OUT join( "\t", @_ ), "\n"; flock( OUT, LOCK_UN ); close(OUT) or die("Cannot close query log: $!"); } );
     my $params = params;
-    $params->{"c"} = "foo" unless(defined($params->{"c"}));
-    $params->{"test"} = "bar" unless($params->{"test"});
+    $params->{"corpus"} = config->{"corpora"}->[0]->{"corpus"} unless ( defined( $params->{"corpus"} ) );
+    data->new();
 };
 
 hook 'before_template' => sub {
     my $tokens = shift;
     $tokens->{"version"} = $VERSION;
-    $tokens->{"home_url"} = uri_for("/");
-    $tokens->{"help_url"} = uri_for("/help");
-    $tokens->{"about_url"} = uri_for("/about");
-    $tokens->{"change_corpus_url"} = uri_for("/change_corpus");
-    $tokens->{"word_form_query_url"} = uri_for("/word_form_query");
-    $tokens->{"lemma_query_url"} = uri_for("/lemma_query");
+
+    # URLs
+    $tokens->{"home_url"}                = uri_for("/");
+    $tokens->{"help_url"}                = uri_for("/help");
+    $tokens->{"about_url"}               = uri_for("/about");
+    $tokens->{"change_corpus_url"}       = uri_for("/change_corpus");
+    $tokens->{"word_form_query_url"}     = uri_for("/word_form_query");
+    $tokens->{"lemma_query_url"}         = uri_for("/lemma_query");
     $tokens->{"complex_query_token_url"} = uri_for("/complex_query_token");
     $tokens->{"complex_query_chunk_url"} = uri_for("/complex_query_chunk");
+
+    # Corpus
+    $tokens->{"url_args"}->{"corpus"} = param("corpus");
 };
 
 get '/' => sub {
-    debug("c=" . param("c"));
-    debug("d=" . param("d")) if(param("d"));
-    debug("test=" . param("test"));
-    template('home', {url_args => [{name => "x", value => "alpha"}, {name => "y", value => "beta"}, {name => "z", value => "gamma"}]});
+    debug( "corpus=" . param("corpus") );
+    template( 'home', { url_args => {"x" => "alpha", "y" => "beta", "z" => "gamma" }});
 };
 
 get '/help' => sub {
