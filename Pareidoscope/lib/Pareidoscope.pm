f3a01b0a572cfde89351710477214e06b8762ee4
Thomas Proisl <thomas.proisl@fau.de>
2012-02-14 07:56:33 +0000

    Mehr Templates werden jetzt richtig befüllt

--- Pareidoscope/lib/Pareidoscope.pm
+++ Pareidoscope/lib/Pareidoscope.pm
@@ -15,6 +15,7 @@ hook 'before' => sub {
     $data = data->new();
     my $params = params;
     $params->{"corpus"} = config->{"corpora"}->[0]->{"corpus"} unless ( defined( $params->{"corpus"} ) );
+    $data->init_corpus( param("corpus") );
 };
 
 hook 'before_template' => sub {
@@ -22,14 +23,14 @@ hook 'before_template' => sub {
     $tokens->{"version"} = $VERSION;
 
     # URLs
-    $tokens->{"home_url"}                = uri_for("/");
-    $tokens->{"help_url"}                = uri_for("/help");
-    $tokens->{"about_url"}               = uri_for("/about");
-    $tokens->{"change_corpus_url"}       = uri_for("/change_corpus");
-    $tokens->{"word_form_query_url"}     = uri_for("/word_form_query");
-    $tokens->{"lemma_query_url"}         = uri_for("/lemma_query");
-    $tokens->{"complex_query_token_url"} = uri_for("/complex_query_token");
-    $tokens->{"complex_query_chunk_url"} = uri_for("/complex_query_chunk");
+    $tokens->{"home_url"}                  = uri_for("/");
+    $tokens->{"help_url"}                  = uri_for("/help");
+    $tokens->{"about_url"}                 = uri_for("/about");
+    $tokens->{"change_corpus_url"}         = uri_for("/change_corpus");
+    $tokens->{"word_form_query_url"}       = uri_for("/word_form_query");
+    $tokens->{"lemma_query_url"}           = uri_for("/lemma_query");
+    $tokens->{"complex_query_token_url"}   = uri_for("/complex_query_token");
+    $tokens->{"complex_query_chunk_url"}   = uri_for("/complex_query_chunk");
     $tokens->{"word_form_results_url"}     = uri_for("/results/word_form_query");
     $tokens->{"lemma_results_url"}         = uri_for("/results/lemma_query");
     $tokens->{"complex_query_results_url"} = uri_for("/results/complex_query");
@@ -40,7 +41,7 @@ hook 'before_template' => sub {
 
 get '/' => sub {
     debug( "corpus=" . param("corpus") );
-    template( 'home', { url_args => {"x" => "alpha", "y" => "beta", "z" => "gamma" }});
+    template('home');
 };
 
 get '/help' => sub {
@@ -52,23 +53,51 @@ get '/about' => sub {
 };
 
 get '/change_corpus' => sub {
-    template('change_corpus');
+    my $beautify = sub { $_[0] = reverse $_[0]; $_[0] =~ s/(\d{3})(?=\d)/$1,/g; return scalar( reverse $_[0] ); };
+    my $yesno = sub { return $_[0] ? "yes" : "no" };
+    my @corpora = map( {
+            "corpus"    => $_->{"corpus"},
+            "name"      => $_->{"display_name"},
+            "tokens"    => $beautify->( $_->{"tokens"} ),
+            "sentences" => $beautify->( $_->{"sentences"} ),
+            "tagset"    => $_->{"tagset"},
+            "chunks"    => $yesno->( $_->{"chunk_ngrams"} ),
+            "subgraphs" => $yesno->( $_->{"subgraphs"} )
+        },
+        grep( $_->{"available"}, @{ config->{"corpora"} } ) );
+    template( 'change_corpus', { "corpora" => \@corpora } );
 };
 
 get '/word_form_query' => sub {
-    template('word_form_query', {});
+    template(
+        'word_form_query',
+        {   "word_classes" => [ "", sort keys %{ config->{"tagsets"}->{ $data->{"active"}->{"tagset"} } } ],
+            "pos_tags"     => $data->{"number_to_tag"}
+        }
+    );
 };
 
 get '/lemma_query' => sub {
-    template('lemma_query');
+    template( 'lemma_query', { "word_classes" => [ sort keys %{ config->{"tagsets"}->{ $data->{"active"}->{"tagset"} } } ] } );
 };
 
 get '/complex_query_token' => sub {
-    template('complex_query_token');
+    template(
+        'complex_query_token',
+        {   "word_classes" => [ "", sort keys %{ config->{"tagsets"}->{ $data->{"active"}->{"tagset"} } } ],
+            "pos_tags"     => $data->{"number_to_tag"}
+        }
+    );
 };
 
 get '/complex_query_chunk' => sub {
-    template('complex_query_chunk');
+    template(
+        'complex_query_chunk',
+        {   "word_classes" => [ "", sort keys %{ config->{"tagsets"}->{ $data->{"active"}->{"tagset"} } } ],
+            "pos_tags"     => $data->{"number_to_tag"},
+            "chunk_types"  => $data->{"number_to_chunk"}
+        }
+    );
 };
 
 any [ 'get', 'post' ] => '/results/word_form_query' => sub {
@@ -82,7 +111,6 @@ any [ 'get', 'post' ] => '/results/lemma_query' => sub {
 any [ 'get', 'post' ] => '/results/complex_query' => sub {
     template('complex_query_results');
 };
-
 
 # get '/' => sub {
 
