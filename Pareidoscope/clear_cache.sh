5e07517f77dd8dcd9b951a3361c5161ebe3a5541
Thomas Proisl <thomas.proisl@fau.de>
2012-08-08 12:36:27 +0000

    clear_cache-Skript löscht keine Dateien im .svn-Verzeichnis mehr

--- Pareidoscope/clear_cache.sh
+++ Pareidoscope/clear_cache.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find public/cache/ -type f -not -name 'queries.log' -print0 | xargs -0 rm
+find public/cache/ -type f -not -wholename '*.svn*' -a -not -name 'queries.log' -print0 | xargs -0 rm
