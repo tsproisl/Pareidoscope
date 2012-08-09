0ab1169cb2c24337d5f8ed8b302d88d29d43593a
Thomas Proisl <thomas.proisl@fau.de>
2012-08-09 06:17:52 +0000

    start-Skript sollte nicht in der Versionskontrolle sein

--- Pareidoscope/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export PERL5LIB=/home/linguistik/tsproisl/local/lib/perl5:/home/linguistik/tsproisl/local/lib/perl5/5.12.3:/home/linguistik/tsproisl/local/lib/perl5/site_perl/5.12.3:$PERL5LIB
-
-bin/app.pl
