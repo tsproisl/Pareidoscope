53ceda098cb74a3796302c9cc6477741cc9e2324
Thomas Proisl <thomas.proisl@fau.de>
2013-11-27 15:19:13 +0000

    Skripte mit aufgenommen

--- setup.py
+++ setup.py
@@ -5,8 +5,17 @@ setup(
     version='0.1.0',
     author='Thomas Proisl',
     author_email='thomas.proisl@fau.de',
-    packages=['pareidoscope','pareidoscope.utils'],
-    # scripts=['bin/foo.py',],
+    packages=[
+        'pareidoscope',
+        'pareidoscope.utils'
+    ],
+    scripts=[
+        'bin/pareidoscope_batch_query',
+        'bin/pareidoscope_create_queries_from_corpus',
+        'bin/pareidoscope_distributed_batch_query_client',
+        'bin/pareidoscope_distributed_batch_query_server',
+        'pareidoscope_random_trees',
+    ],
     # url='http://pypi.python.org/pypi/Pareidoscope/',
     license='GNU General Public License (GPL) 3.0',
     description='A tool for determining the association between arbitrary linguistic structures.',
