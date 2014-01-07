8e06c95ee2507eb3d666c17f622e797f48017519
Thomas Proisl <thomas.proisl@fau.de>
2014-01-07 13:06:09 +0000

    Neues Exportformat

--- pareidoscope/utils/nx_graph.py
+++ pareidoscope/utils/nx_graph.py
@@ -178,6 +178,9 @@ def export_to_cwb_format(nx_graph):
     mapping = {n: i for i, n in enumerate(sorted(nx_graph.nodes()))}
     for v in sorted(nx_graph.nodes()):
         word = nx_graph.node[v]["word"]
+        pos = nx_graph.node[v].get("pos", "X")
+        lemma = nx_graph.node[v].get("lemma", word)
+        wc = nx_graph.node[v].get("wc", "X")
         root = nx_graph.node[v].get("root", "")
         indeps = ["%s(%d,0)" % (l["relation"], mapping[s] - mapping[v]) for s, t, l in nx_graph.in_edges(v, data=True)]
         outdeps = ["%s(0,%d)" % (l["relation"], mapping[t] - mapping[v]) for s, t, l in nx_graph.out_edges(v, data=True)]
@@ -187,8 +190,48 @@ def export_to_cwb_format(nx_graph):
         outdeps = "|" + "|".join(outdeps)
         if len(outdeps) > 1:
             outdeps += "|"
-        output.append([word, "X", word, "X", indeps, outdeps, root])
+        output.append([word, pos, lemma, wc, indeps, outdeps, root])
     return output
+
+
+def export_to_chemical_format(graph, vertice_dict, edge_dict):
+    """Export to chemical format for use with Gaston
+    (http://www.liacs.nl/~snijssen/gaston/index.html).
+    
+    Arguments:
+    - `graph`:
+    - `vertice_dict`:
+    - `edge_dict`:
+
+    """
+    export = ["t # "]
+    vmax = None
+    try:
+        vmax = max(vertice_dict.values())
+    except ValueError:
+        vmax = 0
+    for v in sorted(graph.nodes()):
+        word = graph.node[v]["word"]
+        pos = graph.node[v].get("pos", "X")
+        lemma = graph.node[v].get("lemma", word)
+        wc = graph.node[v].get("wc", "X")
+        t = (word, pos, lemma, wc)
+        if t not in vertice_dict:
+            vmax += 1
+            vertice_dict[t] = vmax
+        export.append("v %d %d" % (v, vertice_dict[t]))
+        emax = None
+    try:
+        emax = max(edge_dict.values())
+    except ValueError:
+        emax = 0
+    for s, t, l in sorted(graph.edges(data=True)):
+        rel = l["relation"]
+        if rel not in edge_dict:
+            emax += 1
+            edge_dict[rel] = emax
+        export.append("e %d %d %d" % (s, t, edge_dict[rel]))
+    return export
 
 
 def is_purely_structural(nx_graph):
@@ -198,7 +241,7 @@ def is_purely_structural(nx_graph):
     Arguments:
     - `query_graph`:
     """
-    return all([_is_unrestricted(edge[2]) for edge in nx_graph.edges(data = True)])
+    return all([_is_unrestricted(edge[2]) for edge in nx_graph.edges(data=True)])
 
 
 def _is_unrestricted(dictionary):
