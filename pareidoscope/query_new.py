33c246aa83ab1993a323de42eab5f1f46bee9a50
Thomas Proisl <thomas.proisl@fau.de>
2016-04-19 12:25:25 +0000

    Summary: Die ganze Datei fliegt wahrscheinlich wieder raus

--- pareidoscope/query_new.py
+++ pareidoscope/query_new.py
@@ -12,73 +12,6 @@ from pareidoscope import subgraph_enumeration
 from pareidoscope import subgraph_isomorphism
 
 
-def sanity_check_c_a_b(gc, ga, gb):
-    """Do a few sanity checks on the query graphs."""
-    connected_c = networkx.is_weakly_connected(gc)
-    connected_a = networkx.is_weakly_connected(ga)
-    connected_b = networkx.is_weakly_connected(gb)
-    vid_c = set([l["vid"] for v, l in gc.nodes(data=True)])
-    vid_a = set([l["vid"] for v, l in ga.nodes(data=True)])
-    vid_b = set([l["vid"] for v, l in gb.nodes(data=True)])
-    # vids are unique
-    vid_c_uniq = gc.number_of_nodes() == len(vid_c)
-    vid_a_uniq = ga.number_of_nodes() == len(vid_a)
-    vid_b_uniq = gb.number_of_nodes() == len(vid_b)
-    # union of vid_a and vid_b is vid_c
-    a_and_b_is_c = vid_a | vid_b == vid_c
-    # ga should subsume gc
-    ga_subsumes_gc = subgraph_enumeration.subsumes_nx(ga, gc)
-    # gb should subsume gc
-    gb_subsumes_gc = subgraph_enumeration.subsumes_nx(gb, gc)
-    sane = all([connected_c, connected_a, connected_b, vid_c_uniq, vid_a_uniq, vid_b_uniq, a_and_b_is_c, ga_subsumes_gc, gb_subsumes_gc])
-    if not sane:
-        raise Exception("Incorrect formulation of query.")
-    return sane
-
-
-def get_n_from_c_a_b(gc, ga, gb):
-    """Derive gn from gc, ga and gb and return it."""
-    vid_to_ga = {l["vid"]: v for v, l in ga.nodes(data=True)}
-    vid_to_gb = {l["vid"]: v for v, l in gb.nodes(data=True)}
-    vid_to_gcn = {l["vid"]: v for v, l in gc.nodes(data=True)}
-    # vn is the intersection of ga and gb
-    vn = set(vid_to_ga.keys()) & set(vid_to_gb.keys())
-    gn = networkx.DiGraph()
-    for vertex, label in gc.nodes(data=True):
-        vid = label["vid"]
-        if vid not in vn:
-            continue
-        # the label is the maximum of the labels in ga and gb
-        label_keys = set(ga.node[vid_to_ga[vid]].keys()) & set(gb.node[vid_to_gb[vid]].keys())
-        if all([ga.node[vid_to_ga[vid]][lk] == gb.node[vid_to_gb[vid]][lk] for lk in label_keys]):
-            gn.add_node(vertex, {lk: ga.node[vid_to_ga[vid]][lk] for lk in label_keys})
-        else:
-            raise Exception("Incompatible vertex labels: %s and  %s" % (repr(ga.node[vid_to_ga[vid]]), repr(gb.node[vid_to_gb[vid]])))
-    # en is the intersection of ea and eb
-    ea = set([(ga.node[s]["vid"], ga.node[t]["vid"]) for s, t in ga.edges()])
-    eb = set([(gb.node[s]["vid"], gb.node[t]["vid"]) for s, t in gb.edges()])
-    en = ea & eb
-    for s, t in en:
-        label_keys = set(ga.edge[vid_to_ga[s]][vid_to_ga[t]].keys()) & set(gb.edge[vid_to_gb[s]][vid_to_gb[t]].keys())
-        if all([ga.edge[vid_to_ga[s]][vid_to_ga[t]][lk] == gb.edge[vid_to_gb[s]][vid_to_gb[t]][lk] for lk in label_keys]):
-            gn.add_edge(vid_to_gcn[s], vid_to_gcn[t], {lk: ga.edge[vid_to_ga[s]][vid_to_ga[t]][lk] for lk in label_keys})
-        else:
-            raise Exception("Incompatible edge labels: %s and  %s" % (repr(ga.edge[vid_to_ga[s]][vid_to_ga[t]]), repr(gb.edge[vid_to_gb[s]][vid_to_gb[t]])))
-    # make sure that the remaining vertices are consecutively labeled
-    gn = nx_graph.ensure_consecutive_vertices(gn)
-    if not networkx.is_weakly_connected(gn):
-        raise Exception("gn is not a connected graph")
-    return gn
-
-
-def strip_vid(graph):
-    """Return a copy of graph without vertex id (vid) labels."""
-    copy = graph.copy()
-    for vertex in copy.nodes():
-        del copy.node[vertex]["vid"]
-    return copy
-
-
 def _get_isomorphism_vertex_candidates(query_graph, normal_candidates, v_target, v_isomorphism, vid_to_iso):
     """Return vertex candidates that are compatible with isomorphism."""
     isomorphism_candidates = [set([vid_to_iso[l["vid"]]]) if l["vid"] in vid_to_iso else v_target - v_isomorphism for v, l in sorted(query_graph.nodes(data=True))]
@@ -133,25 +66,22 @@ def isomorphisms(gc, ga, gb, gn, gs, candidates=None):
     # vid_to_gn = {l["vid"]: v for v, l in gn.nodes(data=True)}
     ct = {x: 0 for x in ["o11", "r1", "c1", "n"]}
     vs = set(gs.nodes())
-    stripped_gc = strip_vid(gc)
-    stripped_ga = strip_vid(ga)
-    stripped_gb = strip_vid(gb)
     normal_cand_c, normal_cand_a, normal_cand_b = None, None, None
-    for isomorphism in subgraph_isomorphism.get_subgraph_isomorphisms_nx(strip_vid(gn), gs, vertex_candidates=candidates):
+    for isomorphism in subgraph_isomorphism.get_subgraph_isomorphisms_nx(gn, gs, vertex_candidates=candidates):
         if normal_cand_c is None:
-            normal_cand_c = nx_graph.get_vertex_candidates(stripped_gc, gs)
+            normal_cand_c = nx_graph.get_vertex_candidates(gc, gs)
         if normal_cand_a is None:
-            normal_cand_a = nx_graph.get_vertex_candidates(stripped_ga, gs)
+            normal_cand_a = nx_graph.get_vertex_candidates(ga, gs)
         if normal_cand_b is None:
-            normal_cand_b = nx_graph.get_vertex_candidates(stripped_gb, gs)
+            normal_cand_b = nx_graph.get_vertex_candidates(gb, gs)
         vid_iso = {gn.node[qv]["vid"]: tv for qv, tv in zip(sorted(gn.nodes()), isomorphism)}
         v_isomorphism = set(isomorphism)
         vert_cand_c = _get_isomorphism_vertex_candidates(gc, normal_cand_c, vs, v_isomorphism, vid_iso)
         vert_cand_a = _get_isomorphism_vertex_candidates(ga, normal_cand_a, vs, v_isomorphism, vid_iso)
         vert_cand_b = _get_isomorphism_vertex_candidates(gb, normal_cand_b, vs, v_isomorphism, vid_iso)
-        args_c = {"query_graph": stripped_gc, "vertex_candidates": vert_cand_c}
-        args_a = {"query_graph": stripped_ga, "vertex_candidates": vert_cand_a}
-        args_b = {"query_graph": stripped_gb, "vertex_candidates": vert_cand_b}
+        args_c = {"query_graph": gc, "vertex_candidates": vert_cand_c}
+        args_a = {"query_graph": ga, "vertex_candidates": vert_cand_a}
+        args_b = {"query_graph": gb, "vertex_candidates": vert_cand_b}
         o11, r1, c1, n = _frequency_signature(functools.partial(subgraph_enumeration.subsumes_nx, target_graph=gs), args_c, args_a, args_b)
         ct["o11"] += o11
         ct["r1"] += r1
