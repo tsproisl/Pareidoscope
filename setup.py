a93bc246c268b6371ad306ca470df87520b2d55f
Thomas Proisl <thomas.proisl@fau.de>
2017-06-03 23:25:38 +0200

    Added some comments explaining the release process

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
+# 1. Upload to PyPI:
 # python3 setup.py sdist
 # python3 setup.py sdist upload
+#
+# 2. Check if everything looks all right: https://pypi.python.org/pypi/Pareidoscope
+#
+# 3. Create tag and push to Github:
+# git tag -a v<version> -m "annotation for this release"
+# git push origin --tags
 
 from os import path
 from setuptools import setup
@@ -8,9 +15,11 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst')) as fh:
     long_description = fh.read()
 
+version = "0.9.2"
+
 setup(
     name='Pareidoscope',
-    version='0.9.1',
+    version=version,
     author='Thomas Proisl',
     author_email='thomas.proisl@fau.de',
     packages=[
@@ -26,6 +35,7 @@ setup(
         'bin/pareidoscope_draw_graphs',
     ],
     url='https://github.com/tsproisl/Pareidoscope',
+    download_url='https://github.com/tsproisl/Pareidoscope/archive/v%s.tar.gz' % version,
     # include_package_data=True,
     # url='http://pypi.python.org/pypi/Pareidoscope/',
     license='GNU General Public License v3 or later (GPLv3+)',
