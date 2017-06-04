3addd98ac1ca725faa2db5702d2b3c30cbc5ecda
Thomas Proisl <thomas.proisl@fau.de>
2017-06-04 10:49:37 +0200

    Adjusted version number

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst')) as fh:
     long_description = fh.read()
 
-version = "0.9.2"
+version = "0.9.4"
 
 setup(
     name='Pareidoscope',
