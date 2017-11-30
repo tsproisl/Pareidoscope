15e35fb11538e86f65c945fe0aa7e33d1e368f3e
Thomas Proisl <thomas.proisl@fau.de>
2017-11-30 11:05:03 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst')) as fh:
     long_description = fh.read()
 
-version = "0.10.2"
+version = "0.11.0"
 
 setup(
     name='Pareidoscope',
